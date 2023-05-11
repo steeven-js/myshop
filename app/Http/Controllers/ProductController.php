e360c92cfcc6861cf26dac7d48770e0d708ab68c
steeven-js <jacques.steeven@gmail.com>
2023-05-11 10:39:38 -0400

    myshop

---
+++ app/Http/Controllers/ProductController.php
@@ -0,0 +1,58 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Cart;
+use App\Models\Product;
+use App\Models\Category;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+
+class ProductController extends Controller
+{
+    //
+    public function index($id = 0)
+    {
+        // Si id != 0 on liste par catégories sinon on liste tout.
+        if ($id != 0) {
+            // Afficher les Product de la catégorie par date de création
+            $products = Product::where('category_id', $id)->orderBy('updated_at', 'desc')->paginate(10);
+        } else {
+            $products = Product::orderBy('updated_at', 'desc')->paginate(10);
+        }
+        $categories = Category::orderBy('name', 'ASC')->get();
+
+        // dd($categories);
+
+        return view('accueil', compact('products', 'categories'));
+    }
+
+    public function detail(Product $product)
+    {
+        // Afficher le détail du produits mais aussi les produits similaires
+
+        $products = Product::where('category_id', $product->category_id)->inRandomOrder()->limit(4)->get();
+        // dd($products);
+
+        return view('detail', compact('product'));
+    }
+
+    /**
+     * Méthode qui permet d'ajouter le produit dans le caddie
+     * Vérifier l'existance du produit
+     * Si il existe. Mettre à jour la quantité
+     */
+    public function addToCart(Product $product)
+    {
+        // On vérifie l'existance du produit du panier
+
+        // SELEC * FROM Cart WHERE user_id="" AND product_id="$product->id" LIMIT(1)
+
+        $cart = Cart::where('user_id', Auth::user()->id)
+            ->where('product_id', $product->id)
+            ->limit(1)
+            ->get();
+
+        dd($cart);
+    }
+}
---
+++ resources/views/detail.blade.php
@@ -0,0 +1,7 @@
+<img class="h-full w-full object-cover object-center" src="{{ Storage::url($product->image) }}" alt="" />
+
+<h3>{{ Str::limit($product->name) }}</h3>
+<P>{{ $product->description }}</P>
+<P>{{ $product->prix }}€</P>
+
+<a href="#">Ajouter au panier</a>
