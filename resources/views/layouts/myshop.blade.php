9d776ba655da3477aad31f5b89ec6a5563b0e50c
steeven-js <jacques.steeven@gmail.com>
2023-05-16 12:05:35 -0400

    first commit

--- resources/views/layouts/myshop.blade.php
+++ resources/views/layouts/myshop.blade.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>My Shop</title>
+    <title>My Shop</title> 
 
         <!-- Web Fonts  -->
 		<link id="googleFonts" href="https://fonts.googleapis.com/css?family=Poppins:300,400,500,600,700,800%7CShadows+Into+Light%7CPlayfair+Display:400&display=swap" rel="stylesheet" type="text/css">
