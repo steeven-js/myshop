17b090e31b8ffc8da7c885cf854c7db8a179e523
steeven-js <jacques.steeven@gmail.com>
2023-07-05 09:49:01 -0400

    refonte db

--- database/migrations/2023_05_29_171540_create_addresses_table.php
+++ database/migrations/2023_05_29_171540_create_addresses_table.php
@@ -12,7 +12,7 @@ return new class extends Migration
     public function up(): void
     {
         Schema::create('addresses', function (Blueprint $table) {
-            $table->bigIncrements('id');
+            $table->id();
             $table->unsignedBigInteger('user_id');
             $table->string('name');
             $table->string('company')->nullable();
--- database/migrations/2023_05_29_172219_create_carriers_table.php
+++ database/migrations/2023_05_29_172219_create_carriers_table.php
@@ -12,7 +12,7 @@ return new class extends Migration
     public function up(): void
     {
         Schema::create('carriers', function (Blueprint $table) {
-            $table->bigIncrements('id');
+            $table->id();
             $table->string('name');
             $table->string('description');
             $table->unsignedDecimal('price', 8, 2);
